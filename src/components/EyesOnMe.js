aa534235a85c9689cc0605eba5448f6a44138603
annetnyabuto <annetgnyabuto@gmail.com>
2025-07-18 08:16:50 +0300

    working with events

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function onButtonFocus(){
+        console.log('Good!');
+    }
+    function onButtonBlur() {
+        console.log('Hey! Eyes on me!');
+    }
+    return (
+        <button onFocus={onButtonFocus} onBlur={onButtonBlur}>
+            Eyes on me
+        </button>
+    );
+}
+
+export default EyesOnMe;
